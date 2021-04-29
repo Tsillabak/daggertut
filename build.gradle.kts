73f04ff68956f09fbfce4e7bbd620366d47d9b12
Kaballist <Darwinmackay@gmail.com>
2021-04-28 19:38:05 -0700

    dagger tut

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+import kotlin.text.Typography.dagger
+
+plugins {
+    java
+    kotlin("jvm") version "1.5.0"
+    kotlin("kapt") version "1.5.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    kapt("com.google.dagger:dagger-compiler:2.35.1")
+    implementation("com.google.dagger:dagger:2.35.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
